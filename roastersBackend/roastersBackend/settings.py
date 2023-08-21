8f94aaf3d4408ba47cd3589aa118d50a88f670de
magcurt08 <magcurt08@magcurt08s-MBP.home>
2023-08-20 21:32:49 -0400

    Adds user table & migrations

--- roastersBackend/roastersBackend/settings.py
+++ roastersBackend/roastersBackend/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'roastersBackend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': env("DATABASE_NAME"),
         'USER': env("DATABASE_USER"),
         'PASSWORD': env("DATABASE_PASSWORD"),
